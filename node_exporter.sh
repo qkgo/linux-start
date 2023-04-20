f6103ce32a4a3f30617aa12befe271f9615a3aae
zhy1 <17908471@qq.com>
2023-04-21 00:48:09 +0800

    Update node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# https://prometheus.io/docs/guides/node-exporter/
-# https://prometheus.io/download/#node_exporter
 
 
+ sudo systemctl stop node_exporter || true
 
+ sudo systemctl disable node_exporter || true
 
 # Set the Node Exporter version to download
 NODE_EXPORTER_VERSION="1.5.0"
@@ -12,10 +12,12 @@ NODE_EXPORTER_VERSION="1.5.0"
 wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
 
 # Extract the binary
-tar xvfz node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+tar -zxvf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+
+chmod +x node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter
 
 # Move the binary to a standard location
-sudo mv node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter /usr/local/bin
+sudo cp node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter /usr/local/bin
 
 # Create a systemd service file for Node Exporter
 echo "[Unit]
@@ -37,6 +39,12 @@ sudo systemctl daemon-reload
 sudo systemctl enable node_exporter
 sudo systemctl start node_exporter
 
+sleep 1
+sudo nohup systemctl status node_exporter . & 
+sleep 1
+
+cat nohup.out
+
 # Verify if the Node Exporter is working
 sleep 5
 wget --spider --quiet http://localhost:9100/metrics
@@ -45,9 +53,18 @@ if [ $? -eq 0 ]; then
     echo "Node Exporter is working correctly"
 else
     echo "Node Exporter is not working, removing the installed files and service"
+    journalctl -u node_exporter
+    sleep 1
     sudo systemctl stop node_exporter
     sudo systemctl disable node_exporter
     sudo rm /etc/systemd/system/node_exporter.service
     sudo rm /usr/local/bin/node_exporter
     sudo systemctl daemon-reload
 fi
+
+### OPTIONs
+rm -rf node_exporter-${NODE_EXPORTER_VERSION}
+
+
+### TODO workat https/tls
+# https://github.com/prometheus/exporter-toolkit/blob/v0.1.0/https/README.md
